80a6e6fcddd26e2f665aa811994ae4ac026eafe2
Kevin Cheng <kcheng@yahoo-inc.com>
2014-09-30 17:50:14 -0700

    Initial Commit

---
+++ ios-course-tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-course-tumblr
+//
+//  Created by Kevin Cheng on 9/30/14.
+//  Copyright (c) 2014 Kevin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
